8867bbf649f7d940092d039ed0e48c55ae92d22f
William Diakite <william.diakite@gmail.com>
2022-08-02 14:33:36 -0400

    add test html

---
+++ experimentations/test-html-display/src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { onMount } from 'svelte'
+
+  let parser = new DOMParser()
+  let chapter_title
+  let section = null
+
+  onMount(async () => {
+    let html = await fetch('data/Test.html').then((res) => res.text())
+    console.log('html', html)
+    let content = parser.parseFromString(html, 'text/html')
+
+    section = content.getElementById('CG01')
+
+    let container = document.getElementById('container')
+    container.appendChild(section)
+  })
+</script>
+
+<h1>Righettino</h1>
+
+<div id="container">
+  <!-- {#if section}
+    {@html section}
+  {/if} -->
+</div>
+
+<style>
+  h3 {
+  }
+</style>
